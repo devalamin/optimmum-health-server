e42d193c24eaecc864bc233f3aa720f6f7fc4328
devalamin <alamin.hasibmd2017@gmail.com>
2022-11-07 23:21:51 +0600

    basic setup

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
+require('dotenv').config();
 const cors = require('cors');
+
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -8,9 +11,25 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+
+const uri = `mongodb+srv://${process.env.DB_USER_OPTIMUM}:${process.env.DB_PASSWORD_OPTIMUM}@cluster0.3b5klku.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+
+
+
 app.get('/', (req, res) => {
     res.send('optimum server running')
 })
+
+
 
 app.listen(port, () => {
     console.log('server running on', port);
