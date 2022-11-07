3b089ac57c35579e8d11f8905615d384c283df1c
devalamin <alamin.hasibmd2017@gmail.com>
2022-11-08 01:11:04 +0600

    get method

--- index.js
+++ index.js
@@ -14,14 +14,31 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER_OPTIMUM}:${process.env.DB_PASSWORD_OPTIMUM}@cluster0.3b5klku.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
+
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
+
+async function run() {
+    try {
+        const servicesCollection = client.db('optimumdb').collection('services')
+
+
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = servicesCollection.find(query)
+            const services = await cursor.toArray();
+            res.send(services)
+        })
+
+    }
+    finally {
+
+    }
+
+
+}
+
+run().catch(err => console.log(err))
 
 
 
